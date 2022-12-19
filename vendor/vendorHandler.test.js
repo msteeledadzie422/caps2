06880cc2cf30657dc9d1531c58ac74630c96b641
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-12-19 02:26:22 -0700

    added tests and a UML to README

--- vendor/vendorHandler.test.js
+++ vendor/vendorHandler.test.js
@@ -1,10 +1,30 @@
 'use strict';
 
-const handleDriver = require('./vendorHandler');
+const eventPool = require('../eventPool');
+const { newOrder, deliveredConfirmation } = require('./vendorHandler');
 
-jest.mock('../hub.js', () => {
+jest.mock('../eventPool.js', () => {
   return {
     on: jest.fn(),
     emit: jest.fn(),
-  }
+  };
+});
+console.log = jest.fn();
+
+describe('Handle Vendor', () => {
+  test('Emit new order pickup as expected', () => {
+    newOrder();
+    expect(console.log).toHaveBeenCalledWith('------------New Package Ready-------------');
+    expect(eventPool.emit).toHaveBeenCalledWith('PICKUP', payload);
+  });
+  test('Emit delivered confirmation as expected', () => {
+    let payload = { 
+      store: 'Walmart',
+      orderID: 'test1234',
+      customer: 'Mandela',
+      address: 'home',
+    }
+    deliveredConfirmation(payload)
+    expect(console.log).toHaveBeenCalledWith(`Thank you, ${payload.customer}.`);
+  });
 });
